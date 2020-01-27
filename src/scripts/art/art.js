61a3a39d8b27fe7c8408d20ab565a0c54cdcec22
Kathryn Wohl <ktwohl@gmail.com>
2020-01-27 14:35:32 -0600

    added fetch and functions to render art info to the DOM

---
+++ src/scripts/art/art.js
@@ -0,0 +1,44 @@
+const getParks = () => {
+    const artURL = "https://data.nashville.gov/resource/eviu-nxp6.json"
+    fetch(artURL)
+        .then(resp => resp.json())
+        .then(parsedArt => {
+            renderArt(parsedArt)
+        });
+}
+
+
+let search = document.getElementById("art__button")
+search.addEventListener("click", function (event) {
+
+})
+
+
+const artFactory = (art) => {
+    if (!art.first_name) {
+        art.first_name = ""
+    }
+
+    if (!art.last_name) {
+        art.last_name = ""
+    }
+
+    return `
+    <section class="artResults">
+    <div><strong>Art Piece:</strong> ${art.artwork}</div>
+    <div><strong>Artist:</strong> ${art.first_name} ${art.last_name}</div>
+    <div><strong>Description:</strong> ${art.description}</div>
+    </section>
+    `
+}
+
+
+
+const renderArt = (art) => {
+    const containter = document.querySelector(".searchResults");
+    art.forEach(art => {
+    const artHtml = artFactory(art);
+    containter.innerHTML += artHtml;
+    });
+}
+// getParks();
