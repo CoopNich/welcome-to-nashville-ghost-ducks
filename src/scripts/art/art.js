ac39795fe8a8996576a9d59060cbebf2556110be
Kathryn Wohl <ktwohl@gmail.com>
2020-01-28 14:14:17 -0600

    fixed search function to be case insensitive, added save button, and added styles to format search results

--- src/scripts/art/art.js
+++ src/scripts/art/art.js
@@ -1,16 +1,16 @@
 const baseURL = "https://data.nashville.gov/resource/eviu-nxp6.json";
 const getArt = {
     searchArt(searchCriteria) {
-    const artURL = baseURL + `?$where=last_name like '%25${searchCriteria}%25'`;
+    const artURL = baseURL + `?$where=upper(last_name) like '%25${searchCriteria.toUpperCase()}%25'`;
     return fetch(artURL)
         .then(resp => resp.json())
         // .then(parsedArt => {
         //     renderArt(parsedArt)
         // });
     }
+    
 }
 
-// if (art.last_name.toUpperCase() == searchCriteria.toUpperCase())
 
 const searchResultsDomManager = {
     artFactory(art) {
@@ -25,18 +25,22 @@ const searchResultsDomManager = {
         if (!art.description) {
             return `
             <section class="artResults">
-            <div><strong>Art Piece:</strong> ${art.artwork}</div>
-            <div><strong>Artist:</strong> ${art.first_name} ${art.last_name}</div>
-            <div><strong>Location:</strong> ${art.location}</div>
+            <h3>Art Piece</h3><p> ${art.artwork}</p>
+            <h3>Artist</h3><p> ${art.first_name} ${art.last_name}</p>
+            <h3>Location</h3><p> ${art.location}</p>
+            <button class="artResults__button">Save</button>
+            <hr>
             </section>
             `
         } else {
             return `
         <section class="artResults">
-        <div><strong>Art Piece:</strong> ${art.artwork}</div>
-        <div><strong>Artist:</strong> ${art.first_name} ${art.last_name}</div>
-        <div><strong>Location:</strong> ${art.location}</div>
-        <div><strong>Description:</strong> ${art.description}</div>
+        <h3>Art Piece</h3><p> ${art.artwork}</p>
+        <h3>Artist</h3><p> ${art.first_name} ${art.last_name}</p>
+        <h3>Location</h3><p> ${art.location}</p>
+        <h3>Description</h3><p> ${art.description}</p>
+        <button class="artResults__button">Save</button>
+        <hr>
         </section>
         `
         }
