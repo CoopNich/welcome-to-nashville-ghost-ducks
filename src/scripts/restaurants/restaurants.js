d7485087b17e2bd82d088fb11ea71734d85b73c0
Cooper Nichols <coopnich92@gmail.com>
2020-01-28 10:53:27 -0600

    render results to DOM

--- src/scripts/restaurants/restaurants.js
+++ src/scripts/restaurants/restaurants.js
@@ -1,77 +1,47 @@
-// restaurantsAPIManager
 
-
-const apiBaseUrl = "https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating&apikey=b8fa3a978e54fb37bb99b79308ab00b3";
-
-const apiManager = {
-  searchRestaurants(searchCriteria) {
-
-    console.log("searchRestaurants");
-
-    const criteria = encodeURIComponent(`"%${searchCriteria.toUpperCase()}%"`);
-    const url = apiBaseUrl + `$where=route_name like ${criteria}`;
-    return fetch(url).then(resp => resp.json());
-  }
+const restaurantsAPIManager = {
+    searchRestaurants(restaurantCuisine) {
+        const apiURL = `https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=${restaurantCuisine}&cuisines=${restaurantCuisine}&apikey=b8fa3a978e54fb37bb99b79308ab00b3`;
+    //    console.log(apiURL)
+        return fetch(apiURL)
+            .then(resp => resp.json())
+    }
+    
 };
 
-// restaurantsSearchEventsManager
-
-const searchEventManager = {
-  addSearchClickEventListener() {
-    console.log("addSearchClickEventListener");
-
-    const button = document.getElementById("search-btn");
-
-    button.addEventListener("click", () => {
-
-      console.log("button click handler");
-
-      const input = document.getElementById("search-criteria");
-      const searchCriteria = input.value;
-      const searchResultPromise = apiManager.searchBusStops(searchCriteria);
-      searchResultPromise.then(searchResults => {
-        searchResultsDomManager.renderSearchResults(searchResults);
-      });
-    });
-  }
+const restaurantResultsDomManager = {
+    restaurantFactory(restaurant) {
+        return `
+        <div class="restaurant">
+            <h3> Name: ${restaurant.restaurant.name}</h3>
+            <p>Address: ${restaurant.restaurant.location.address}</p>
+        </div>
+        `;
+    },
+    renderRestaurantResults(restaurantsResponse) {
+        // console.log(restaurants)
+        const restaurants = restaurantsResponse.restaurants
+        const container = document.querySelector(".searchResults");
+        container.innerHTML = "";
+        for (let i=0; i<restaurants.length; i++) {
+            // restauraunts[i] represents current restaurant in the array being looped over
+            // the following line plugs the current restaurant into the HTML factory and renders it to the DOM in the .searchResults container
+            container.innerHTML += this.restaurantFactory(restaurants[i]);}
+    }
 }
 
-// restaurantsDOMManager
-
-const searchResultsDomManager = {
-  busStopFactory(busStop, index) {
-    console.log("busStopFactory");
-
-    // We use the "index" to make unique ids for each bus stop
-    return `
-      <section id="busstop-${index}" class="bus-stop">
-        <div id="routename-${index}" class="bus-stop__route-name">
-          ${busStop.route_name}
-        </div>
-        <div id="stopname-${index}" class="bus-stop__stopname">
-          ${busStop.stopname}
-        </div>
-        <button id="favorite-${index}" class="bus-stop__favorite">
-          Favorite &#11088;
-        </button>
-      </section>
-    `;
-  },
-  renderSearchResults(searchResults) {
-
-    console.log("renderSearchResults");
-
-    favoriteEventManager.removeFavoriteEventListeners();
-
-    const container = document.querySelector("#search-results");
-    container.innerHTML = "";
-
-    for (let i=0; i<searchResults.length; i++) {
-      const busStop = searchResults[i];
-      container.innerHTML += this.busStopFactory(busStop, i);
+const restaurantResultManager = {
+    addSearchClickEventListener() {
+        const button = document.getElementById("restaurant-search-button");
+        button.addEventListener("click", () => {
+            const input = document.getElementById("restaurant-search-criteria");
+            const restaurantCuisine = input.value;
+            const restaurantSearchPromise = restaurantsAPIManager.searchRestaurants(restaurantCuisine);
+            restaurantSearchPromise.then(cuisine => {
+                restaurantResultsDomManager.renderRestaurantResults(cuisine);
+            });
+        });
     }
+}
 
-    favoriteEventManager.addFavoriteEventListeners();
-  }
-};
-
+restaurantResultManager.addSearchClickEventListener();
