6b5d22b8fa436b462911a553fb47d12ad1fc0bcc
Kathryn Wohl <ktwohl@gmail.com>
2020-01-28 10:56:14 -0600

    added search function that returns data based on search for artist's last name

--- src/scripts/art/art.js
+++ src/scripts/art/art.js
@@ -1,63 +1,113 @@
-const getArt = () => {
-    const baseURL = "https://data.nashville.gov/resource/eviu-nxp6.json"
-    const artURL = baseURL + ``
-    fetch(artURL)
+const baseURL = "https://data.nashville.gov/resource/eviu-nxp6.json";
+const getArt = {
+    searchArt(searchCriteria) {
+    const artURL = baseURL + `?$where=last_name like '%25${searchCriteria}%25'`;
+    return fetch(artURL)
         .then(resp => resp.json())
-        .then(parsedArt => {
-            renderArt(parsedArt)
+        // .then(parsedArt => {
+        //     renderArt(parsedArt)
+        // });
+    }
+}
+
+// if (art.last_name.toUpperCase() == searchCriteria.toUpperCase())
+
+const searchResultsDomManager = {
+    artFactory(art) {
+        if (!art.first_name) {
+            art.first_name = ""
+        }
+    
+        if (!art.last_name) {
+            art.last_name = ""
+        }
+    
+        if (!art.description) {
+            return `
+            <section class="artResults">
+            <div><strong>Art Piece:</strong> ${art.artwork}</div>
+            <div><strong>Artist:</strong> ${art.first_name} ${art.last_name}</div>
+            <div><strong>Location:</strong> ${art.location}</div>
+            </section>
+            `
+        } else {
+            return `
+        <section class="artResults">
+        <div><strong>Art Piece:</strong> ${art.artwork}</div>
+        <div><strong>Artist:</strong> ${art.first_name} ${art.last_name}</div>
+        <div><strong>Location:</strong> ${art.location}</div>
+        <div><strong>Description:</strong> ${art.description}</div>
+        </section>
+        `
+        }
+    },
+    renderArt(searchResults) {
+        const containter = document.querySelector(".searchResults");
+        containter.innerHTML = "";
+
+        // for (let i = 0; i < searchResults.length; i++) {
+        //     const artHtml = this.artFactory(searchResults[i]);
+        //     containter.innerHTML += artHtml
+        // }
+
+        searchResults.forEach(art => {
+        const artHtml = this.artFactory(art);
+        containter.innerHTML += artHtml;
         });
+    }
+
 }
 
 const searchEventManager = {
 
     addSearchClickEventListener() {
-        console.log("add search click event test")
+        console.log("added search click event listener")
+
         const search = document.getElementById("art__button")
+
         search.addEventListener("click", () => {
+
             console.log("button clicked")
+
             const input = document.getElementById("art__input")
             const searchCriteria = input.value 
-            const searchResultPromise = getArt
-        })
-    }
-}
-
-const artFactory = (art) => {
-    if (!art.first_name) {
-        art.first_name = ""
-    }
-
-    if (!art.last_name) {
-        art.last_name = ""
-    }
-
-    if (!art.description) {
-        return `
-        <section class="artResults">
-        <div><strong>Art Piece:</strong> ${art.artwork}</div>
-        <div><strong>Artist:</strong> ${art.first_name} ${art.last_name}</div>
-        <div><strong>Location:</strong> ${art.location}</div>
-        </section>
-        `
-    } else {
-        return `
-    <section class="artResults">
-    <div><strong>Art Piece:</strong> ${art.artwork}</div>
-    <div><strong>Artist:</strong> ${art.first_name} ${art.last_name}</div>
-    <div><strong>Location:</strong> ${art.location}</div>
-    <div><strong>Description:</strong> ${art.description}</div>
-    </section>
-    `
+            const searchResultPromise = getArt.searchArt(searchCriteria)
+            searchResultPromise.then(searchResults => {
+                searchResultsDomManager.renderArt(searchResults)
+            });
+        });
     }
 }
 
 
+searchEventManager.addSearchClickEventListener()
 
-const renderArt = (art) => {
-    const containter = document.querySelector(".searchResults");
-    art.forEach(art => {
-    const artHtml = artFactory(art);
-    containter.innerHTML += artHtml;
-    });
-}
-getArt();
+
+// const artFactory = (art) => {
+//     if (!art.first_name) {
+//         art.first_name = ""
+//     }
+
+//     if (!art.last_name) {
+//         art.last_name = ""
+//     }
+
+//     if (!art.description) {
+//         return `
+//         <section class="artResults">
+//         <div><strong>Art Piece:</strong> ${art.artwork}</div>
+//         <div><strong>Artist:</strong> ${art.first_name} ${art.last_name}</div>
+//         <div><strong>Location:</strong> ${art.location}</div>
+//         </section>
+//         `
+//     } else {
+//         return `
+//     <section class="artResults">
+//     <div><strong>Art Piece:</strong> ${art.artwork}</div>
+//     <div><strong>Artist:</strong> ${art.first_name} ${art.last_name}</div>
+//     <div><strong>Location:</strong> ${art.location}</div>
+//     <div><strong>Description:</strong> ${art.description}</div>
+//     </section>
+//     `
+//     }
+// }
