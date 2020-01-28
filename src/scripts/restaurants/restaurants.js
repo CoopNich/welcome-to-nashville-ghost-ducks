f0b863bf49ac9667a046c54be3d5a3d9df604fc9
Cooper Nichols <coopnich92@gmail.com>
2020-01-28 11:15:28 -0600

    rendered results to DOM

--- src/scripts/restaurants/restaurants.js
+++ src/scripts/restaurants/restaurants.js
@@ -1,7 +1,7 @@
 
 const restaurantsAPIManager = {
     searchRestaurants(restaurantCuisine) {
-        const apiURL = `https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=${restaurantCuisine}&cuisines=${restaurantCuisine}&apikey=b8fa3a978e54fb37bb99b79308ab00b3`;
+        const apiURL = `https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=${restaurantCuisine}&cuisines=${restaurantCuisine}&apikey=${restaurantsAPIKey}`;
     //    console.log(apiURL)
         return fetch(apiURL)
             .then(resp => resp.json())
