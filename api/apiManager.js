ed3b3f44ac1f4268c690cdf9bbdd234b05faa790
Matthew Kroeger <mattk932009@gmail.com>
2020-01-29 17:24:07 -0600

    added extensive comments to help explain

--- api/apiManager.js
+++ api/apiManager.js
@@ -1,21 +1,30 @@
 const apiManager = {
+    //Searches through our existing itinerary.json file to see what is in the itinerary at any given time
     searchItinerary() {
         return fetch(`http://localhost:8088/itinerary`)
             .then(response => response.json());
     },
+
+    //This put method is to put a concert(text) into the itinerary.json so that it is saved in the itinerary for all refreshes
     putItineraryConcert(text) {
+        //We call the itinerary function we just set above to see what is CURRENTLY in the itinerary before changing a value
         const jsonPromise = this.searchItinerary()
         jsonPromise.then(feature => {
+        /*After recieving the CURRENT itinerary, since this function for adding a concert I save every other value so that I don't lose them*/
             const park = feature.park
             const art = feature.art
             const rest = feature.restaurant
+        /*Now that the other 3 values in our itinerary are saved I want to update the current itinerary with the 3 same values and my new concert*/
         fetch(`http://localhost:8088/itinerary`, {
             method: 'PUT',
             body: JSON.stringify({
+                //keeps id
                 "id": 1,
+                //keeps other 3 values the same
                 "park": park,
                 "art": art,
                 "restaurant": rest,
+                /*concert takes the (text) in the very beginnging of the function since it is the only one changing*/
                 "concert": text
             }),
             headers: {
@@ -23,9 +32,11 @@ const apiManager = {
             }
         })
             .then(response => response.json())
+            /*this print function call updates the DOM itinerary with our CHANGED value*/
             .then(json => this.printToDOM())
         })
     },
+    //This function prints out our itinerary on the DOM
     printToDOM() {
         const parks = document.querySelector(".itinerary__parks")
         const art = document.getElementById("itinerary__art")
