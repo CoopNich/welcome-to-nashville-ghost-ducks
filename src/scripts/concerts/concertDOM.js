cfe601f8458915a58cd58be3b00456336f62db3d
Matthew Kroeger <mattk932009@gmail.com>
2020-01-29 10:36:53 -0600

    added help search functionality

--- src/scripts/concerts/concertDOM.js
+++ src/scripts/concerts/concertDOM.js
@@ -10,21 +10,36 @@ const concertResultsDomManager = {
         `;
     },
     renderconcertResults(concerts) {
-        saveEventManager.removeSaveEventListeners();
+        saveConcertEventManager.removeSaveEventListeners();
         const container = document.querySelector(".searchResults");
         container.innerHTML = "<h3>Concert Results:</h3>";
         //conditional to catch for no match searches
         if (concerts.length == 0) {
-            container.innerHTML += "<h3>No showings! Try searching another genre.</h3>"
+            container.innerHTML += "<h3>No showings! Try searching another genre, or search help for a list of genres playing.</h3>"
         }
         for (let i = 0; i < concerts.length; i++) {
             container.innerHTML += this.concertFactory(concerts[i], (i + 1));
         };
-        saveEventManager.addSaveEventListeners();
+        saveConcertEventManager.addSaveEventListeners();
     },
     clearSearchResults() {
         const container = document.querySelector(".searchResults");
         container.innerHTML = " ";
+    },
+    helpSearch() {
+        const container = document.querySelector(".searchResults");
+        container.innerHTML = `
+        <h3>The current genres showing with tickets available to purchase are: </h3>
+        <ul class="genreList">
+        <li>Comedy</li>
+        <li>Rock</li>
+        <li>Pop</li>
+        <li>R&B</li>
+        <li>Country</li>
+        <li>Hip-Hop/Rap</li>
+        <li>Metal</li>
+        </ul>
+        `
     }
 
 };
